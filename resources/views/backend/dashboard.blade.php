78823e786b4b00725032b003f120eaa4ce44ce15
Moch. Lutfi <kapten.lufi@gmail.com>
2017-06-02 11:27:22 +0800

    Show map on dashboard

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -8,17 +8,17 @@
 @endsection
 
 @section('content')
-    <div class="box box-success">
-        <div class="box-header with-border">
-            <h3 class="box-title">{{ trans('strings.backend.dashboard.welcome') }} {{ $logged_in_user->name }}!</h3>
+    <div class="box box-info">
+            <div class="box-header with-border">
+            <h3 class="box-title">{{ trans('strings.backend.map.label') }}</h3>
             <div class="box-tools pull-right">
                 <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
             </div><!-- /.box tools -->
         </div><!-- /.box-header -->
         <div class="box-body">
-            {!! trans('strings.backend.welcome') !!}
+            <div id="map"></div>
         </div><!-- /.box-body -->
-    </div><!--box box-success-->
+    </div>
 
     <div class="box box-info">
         <div class="box-header with-border">
@@ -31,4 +31,19 @@
             {!! history()->render() !!}
         </div><!-- /.box-body -->
     </div><!--box box-success-->
+@endsection
+
+@section('after-scripts')
+    <script>
+      function initMap() {
+        var malaysia = {lat: 4.2105, lng: 101.9758};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: malaysia
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key={!! getenv('GOOGLE_MAP_API') !!}&callback=initMap">
+    </script>
 @endsection
