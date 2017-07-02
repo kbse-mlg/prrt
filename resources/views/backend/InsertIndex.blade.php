41aa852428b61f3e8eb3e8f98aca76a63bfc99b3
restu <restu@kbse.com.my>
2017-07-02 22:10:44 +0800

    changing

--- resources/views/backend/InsertIndex.blade.php
+++ resources/views/backend/InsertIndex.blade.php
@@ -1,19 +1,35 @@
 @extends('backend.layouts.app')
 @section('content')
 
-  <div class="row">
+<div class="row">
     <div class="col-md-12">
-    <a href="{{ route('admin.insert') }}" class="btn btn-primary">Tambah</a>
-        <table class="table">
-        <tr>
-           <th>No</th>
-           <th>Building</th>
-           <th>Alamat</th>
-           <th>Latitude</th>
-           <th>Latitude</th>
-        </tr>
-        <tbody>
-           <?php $i = 1 ?>
+        <a href="{{ route('admin.insert') }}" class="btn btn-primary">Tambah</a>
+    <div class="box">
+    <div class="box-header">
+        <h3 class="box-title">List Building</h3>
+        <div class="box-tools">
+            <div class="input-group">
+                <input type="text" style="width:150px" name="table_search" class="form-control input-sm pull-right" placeholder="Search" value="">
+                <div class="input-group-btn">
+                    <button class="btn btn-sm btn-default">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="box-body table-responsive no-padding">
+        <table class="table table-hover">
+            <tbody>
+                <tr>
+                    
+                    <th>No</th>
+                    <th>Building</th>
+                    <th>Alamat</th>
+                    <th>Latitude</th>
+                    <th>Latitude</th>
+                </tr>
+                <?php $i = 1 ?>
             @foreach($user as $item)
             <tr>
                 <td>{{$i}}</td>
@@ -24,9 +40,11 @@
             </tr>
             <?php $i++; ?>
             @endforeach
-        </tbody>
+            </tbody>
         </table>
     </div>
-  </div>
+</div>
+    </div>
+</div>
  
 @endsection
