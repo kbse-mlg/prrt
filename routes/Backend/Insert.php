143832d6e4d55dd3325750feb16de942db714d23
restu <restu@kbse.com.my>
2017-07-07 01:23:07 +0800

    biasakan sering commit tu

--- routes/Backend/Insert.php
+++ routes/Backend/Insert.php
@@ -3,3 +3,5 @@ Route::get('tambah', 'InsertController@index')->name('tambah');
 Route::get('insert','InsertController@add')->name('insert');
 Route::post('building/','InsertController@insertBuilding')->name('insertBuilding');
 Route::get('facility','InsertController@addFacility')->name('addFacility');
+Route::get('rumah','InsertController@rumahIndex')->name('indexRumah');
+Route::get('penduduk','InsertController@pendudukIndex')->name('pendudukIndex');
