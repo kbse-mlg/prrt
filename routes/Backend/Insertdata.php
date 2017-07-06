2cd349091d4596ec9b702c56d7c9b69e2c5aa57a
restu <restu@kbse.com.my>
2017-07-07 01:13:23 +0800

    commit

--- routes/Backend/Insertdata.php
+++
@@ -1,7 +0,0 @@
-<?php
-Route::get('tambah', 'InsertData@index')->name('tambah');
-Route::get('insert','InsertData@add')->name('insert');
-Route::post('building/','InsertData@insertBuilding')->name('insertBuilding');
-Route::get('facility','InsertData@addFacility')->name('addFacility');
-Route::get('rumah','InsertData@rumahIndex')->name('indexRumah');
-Route::get('penduduk','InsertData@pendudukIndex')->name('pendudukIndex');
